80ffe042512c4ddcc7229f5b9317e9620626a116
Frank Xu <frank.xu@northbright.com>
2016-06-12 18:05:11 +0800

    Add ExampleClient_Post() and ExampleClient_Exec()

--- client_test.go
+++ client_test.go
@@ -8,19 +8,19 @@ import (
 	"github.com/northbright/alidayu"
 )
 
-var (
-	MyAppKey    string = "" // App Key.
-	MyAppSecret string = "" // App Secret.
-	// Send Verification Code in SMS.
-	MySignName     string = "注册验证" // SMS Sign Name. Ex: "注册验证".
-	MyTemplateCode string = ""     // SMS Template Code. Ex: "SMS_XXXXXX".
-	MyPhoneNumber  string = ""     // Phone number to send SMS. Ex: "13800138000".
-	// Send Verification Code in Single Call.
-	MyShowNumber string = "" // Show Number. Ex: "051XXXXXX".
-	MyTTSCode    string = "" // TTS Code. Ex: "TTS_XXXXXXX".
-)
+func ExampleClient_Post() {
+	var (
+		MyAppKey    string = "" // App Key.
+		MyAppSecret string = "" // App Secret.
+		// Send Verification Code in SMS.
+		MySignName     string = "注册验证" // SMS Sign Name. Ex: "注册验证".
+		MyTemplateCode string = ""     // SMS Template Code. Ex: "SMS_XXXXXX".
+		MyPhoneNumber  string = ""     // Phone number to send SMS. Ex: "13800138000".
+		// Send Verification Code in Single Call.
+		MyShowNumber string = "" // Show Number. Ex: "051XXXXXX".
+		MyTTSCode    string = "" // TTS Code. Ex: "TTS_XXXXXXX".
+	)
 
-func Example() {
 	// Create a new client.
 	c := &alidayu.Client{AppKey: MyAppKey, AppSecret: MyAppSecret, UseHTTPS: false}
 
@@ -57,7 +57,6 @@ func Example() {
 	// ------------------------------------------
 	// Send Verification Code in Single Call.
 	// ------------------------------------------
-
 	// Set Parameters.
 	params = map[string]string{}
 	params["method"] = "alibaba.aliqin.fc.tts.num.singlecall"     // Set method to make single call.
@@ -82,6 +81,69 @@ func Example() {
 	}
 
 	fmt.Fprintf(os.Stderr, "c.Post() successfully\n%v\n", string(data2))
+
+	// Output:
+}
+
+func ExampleCient_Exec() {
+	var (
+		MyAppKey    string = "" // App Key.
+		MyAppSecret string = "" // App Secret.
+		// Send Verification Code in SMS.
+		MySignName     string = "注册验证" // SMS Sign Name. Ex: "注册验证".
+		MyTemplateCode string = ""     // SMS Template Code. Ex: "SMS_XXXXXX".
+		MyPhoneNumber  string = ""     // Phone number to send SMS. Ex: "13800138000".
+		// Send Verification Code in Single Call.
+		MyShowNumber string = "" // Show Number. Ex: "051XXXXXX".
+		MyTTSCode    string = "" // TTS Code. Ex: "TTS_XXXXXXX".
+	)
+
+	// Create a new client.
+	c := &alidayu.Client{AppKey: MyAppKey, AppSecret: MyAppSecret, UseHTTPS: false}
+
+	// ---------------------------------------
+	// Send Verification Code in SMS.
+	// ---------------------------------------
+
+	// Set Parameters.
+	params := map[string]string{}
+	params["format"] = "json"
+	params["method"] = "alibaba.aliqin.fc.sms.num.send"           // Set method to send SMS.
+	params["extend"] = "123456"                                   // Set callback parameter.
+	params["sms_type"] = "normal"                                 // Set SMS type.
+	params["sms_free_sign_name"] = MySignName                     // Set SMS signature.
+	params["sms_param"] = `{"code":"123456", "product":"My App"}` // Set variable for SMS template.
+	params["sms_template_code"] = MyTemplateCode                  // Set SMS template code.
+	params["rec_num"] = MyPhoneNumber                             // Set phone number to send SMS.
+
+	// Call Exec() to post the request.
+	success, result, err := c.Exec(params)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "c.Exec() error: %v\nsuccess: %v\nresult: %v\n", err, success, result)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "c.Exec() successfully\nsuccess: %v\nresult: %s\n", success, result)
+
+	// ------------------------------------------
+	// Send Verification Code in Single Call.
+	// ------------------------------------------
+	// Set Parameters.
+	params = map[string]string{}
+	params["method"] = "alibaba.aliqin.fc.tts.num.singlecall"     // Set method to make single call.
+	params["tts_param"] = `{"code":"123456", "product":"My App"}` // Set variable for TTS template.
+	params["called_num"] = MyPhoneNumber                          // Set phone number to make single call.
+	params["called_show_num"] = MyShowNumber                      // Set show number.
+	params["tts_code"] = MyTTSCode                                // Set TTS code.
+
+	// Call Exec() to post the request.
+	success, result, err = c.Exec(params)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "c.Exec() error: %v\nsuccess: %v\nresult: %v\n", err, success, result)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "c.Exec() successfully\nsuccess: %v\nresult: %s\n", success, result)
 
 	// Output:
 }
