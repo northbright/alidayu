9ac756c0f2fc8d8153e587613d9377a28758c5c3
Frank Xu <frank.xu@northbright.com>
2016-10-21 14:01:56 +0800

    Fix golint warnings in comments.

--- client.go
+++ client.go
@@ -15,17 +15,19 @@ import (
 )
 
 const (
-	// REST API URL of alidayu
-	HTTP_URL  string = "http://gw.api.taobao.com/router/rest"
+	// HTTP_URL is for HTTP REST API URL.
+	HTTP_URL string = "http://gw.api.taobao.com/router/rest"
+	// HTTPS_URL is for HTTPS REST API URL.
 	HTTPS_URL string = "https://eco.taobao.com/router/rest"
 
-	// Success Tags in Response String
+	// SUCCESS_TAG_JSON is success tag for JSON in response string.
 	SUCCESS_TAG_JSON string = `"success":true`
-	SUCCESS_TAG_XML  string = `<success>true</success>`
+	// SUCCESS_TAG_XML is sucess tag for XML in response string
+	SUCCESS_TAG_XML string = `<success>true</success>`
 )
 
 var (
-	// Default Common Parameters of alidayu APIs
+	// DefCommonParams is default common parameters of alidayu APIs
 	DefCommonParams map[string]string = map[string]string{
 		"format":      "json", // Response Format("json" or "xml")
 		"v":           "2.0",  // API version("2.0")
@@ -41,7 +43,7 @@ type Client struct {
 	http.Client
 }
 
-// IsValid() checks if a client is valid.
+// IsValid checks if a client is valid.
 func (c *Client) IsValid() bool {
 	if c.AppKey == "" || c.AppSecret == "" {
 		return false
@@ -50,7 +52,7 @@ func (c *Client) IsValid() bool {
 	return true
 }
 
-// UpdateCommonParams() updates the given parameters by merge default common parameters.
+// UpdateCommonParams updates the given parameters by merge default common parameters.
 func (c *Client) UpdateCommonParams(params map[string]string) {
 	for k, v := range DefCommonParams {
 		if _, ok := params[k]; !ok {
@@ -66,7 +68,7 @@ func (c *Client) UpdateCommonParams(params map[string]string) {
 	params["timestamp"] = fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
 
-// GetSortedQueryStr() sorts the keys of parameters and generate sorted query string which can be used to sign(MD5 or HMAC_MD5).
+// GetSortedQueryStr sorts the keys of parameters and generate sorted query string which can be used to sign(MD5 or HMAC_MD5).
 func GetSortedQueryStr(params map[string]string) (sortedQueryStr string) {
 	keys := []string{}
 	for k, _ := range params {
@@ -81,7 +83,7 @@ func GetSortedQueryStr(params map[string]string) (sortedQueryStr string) {
 	return str
 }
 
-// SignMD5() gets the sorted query string and sign it using MD5.
+// SignMD5 gets the sorted query string and sign it using MD5.
 func (c *Client) SignMD5(params map[string]string) (signature string) {
 	// Sort keys by name to generate MD5 hash.
 	// See details at:
@@ -92,7 +94,7 @@ func (c *Client) SignMD5(params map[string]string) (signature string) {
 
 }
 
-// SignHMAC() gets the sorted query string and sign it using HMAC_MD5.
+// SignHMAC gets the sorted query string and sign it using HMAC_MD5.
 func (c *Client) SignHMAC(params map[string]string) (signature string) {
 	// Sort keys by name to generate HMAC_MD5 hash.
 	// See details at:
@@ -105,7 +107,7 @@ func (c *Client) SignHMAC(params map[string]string) (signature string) {
 	return fmt.Sprintf("%X", mac.Sum(nil))
 }
 
-// MakeRequestBody() makes the HTTP request body by given parameters for each REST API.
+// MakeRequestBody makes the HTTP request body by given parameters for each REST API.
 func (c *Client) MakeRequestBody(params map[string]string) (body io.Reader, err error) {
 	if !c.IsValid() {
 		return nil, errors.New("Empty App Key or App Secret.")
@@ -145,7 +147,7 @@ func (c *Client) MakeRequestBody(params map[string]string) (body io.Reader, err 
 	return strings.NewReader(values.Encode()), nil
 }
 
-// Post() does the HTTP post for each REST API.
+// Post does the HTTP post for each REST API.
 //
 //   Params:
 //     params: map that contains parameters of REST API. See official docs to fill the parameters.
@@ -174,7 +176,7 @@ func (c *Client) Post(params map[string]string) (resp *http.Response, err error)
 	return c.Do(req)
 }
 
-// Exec() executes the REST API and get the response. It's a wrapper of Post().
+// Exec executes the REST API and get the response. It's a wrapper of Post().
 //
 //   Params:
 //     params: map that contains parameters of REST API. See official docs to fill the parameters.
